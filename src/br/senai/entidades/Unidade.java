cf41b827289c322e49946f3e7d751631668cfd68
Renan Souza <falecomrenansouza@gmail.com>
2017-06-21 03:16:16 -0300

    Classes modificadas, imagens adicionadas

--- src/br/senai/entidades/Unidade.java
+++ src/br/senai/entidades/Unidade.java
@@ -2,4 +2,7 @@ package br.senai.entidades;
 
 public class Unidade {
 
+	public Integer id;
+	public String nome;
+	
 }
--- src/br/senai/entidades/Usuario.java
+++ src/br/senai/entidades/Usuario.java
@@ -2,4 +2,62 @@ package br.senai.entidades;
 
 public class Usuario {
 
+	// Campos referente aos atributos do usuário
+	
+	private Integer matricula;
+	private String nome;
+	private String login;
+	private String senha;
+	
+	// Getters e Setters
+	
+	public Integer getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		return true;
+	}
+	W
 }
